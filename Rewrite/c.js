af335d565779ed2b6df9a0c15115c07a0c965038
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-09-12 10:43:30 +0300

    Update c.js

---
+++ Rewrite/c.js
@@ -0,0 +1,419 @@
+textplus/00476 CFNetwork/1568.100.1 Darwin/24.0.0
+{
+    "product_entitlement_mapping" =     {
+        "TPFTC_CA_JAN2013_CRDT_1099_9.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_CA_JAN2013_CRDT_1099_9.99";
+        };
+        "TPFTC_CA_JAN2013_CRDT_199_1.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_CA_JAN2013_CRDT_199_1.99";
+        };
+        "TPFTC_CA_JAN2013_CRDT_2199_19.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_CA_JAN2013_CRDT_2199_19.99";
+        };
+        "TPFTC_CA_JAN2013_CRDT_99_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_CA_JAN2013_CRDT_99_0.99";
+        };
+        "TPFTC_CA_OCT2012_MINS_1000_19.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_CA_OCT2012_MINS_1000_19.99";
+        };
+        "TPFTC_CA_OCT2012_MINS_400_9.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_CA_OCT2012_MINS_400_9.99";
+        };
+        "TPFTC_CA_OCT2012_MINS_75_1.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_CA_OCT2012_MINS_75_1.99";
+        };
+        "TPFTC_I_CA_APR2013_CRDT_549_4.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_I_CA_APR2013_CRDT_549_4.99";
+        };
+        "TPFTC_I_CA_APR2014_UNLCALLUS_1MO_2.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_CA_APR2014_UNLCALLUS_1MO_2.99";
+        };
+        "TPFTC_I_CA_JUL2014_UNLCALLMXALL_1MO_29.99" =         {
+            entitlements =             (
+                "unlimited_calling_mx"
+            );
+            "product_identifier" = "TPFTC_I_CA_JUL2014_UNLCALLMXALL_1MO_29.99";
+        };
+        "TPFTC_I_CA_JUL2014_UNLCALLUS_1MO_1.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_CA_JUL2014_UNLCALLUS_1MO_1.99";
+        };
+        "TPFTC_I_CA_JUN2014_ADFREEUNLCALLUS_1MO_3.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_I_CA_JUN2014_ADFREEUNLCALLUS_1MO_3.99";
+        };
+        "TPFTC_I_CA_MAY2014_UNLCALLMX_1MO_6.99" =         {
+            entitlements =             (
+                "unlimited_calling_mx_landline"
+            );
+            "product_identifier" = "TPFTC_I_CA_MAY2014_UNLCALLMX_1MO_6.99";
+        };
+        "TPFTC_I_CA_MAY2014_UNLCALLUS_1MO_4.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_CA_MAY2014_UNLCALLUS_1MO_4.99";
+        };
+        "TPFTC_I_CA_MAY2014_UNLCALLUS_1YR_19.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_CA_MAY2014_UNLCALLUS_1YR_19.99";
+        };
+        "TPFTC_I_CA_MAY2014_UNLCALLUS_1YR_29.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_CA_MAY2014_UNLCALLUS_1YR_29.99";
+        };
+        "TPFTC_I_CA_NOV2014_UNLCALLUS_1YR_9.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_CA_NOV2014_UNLCALLUS_1YR_9.99";
+        };
+        "TPFTC_I_US_APR2013_CRDT_549_4.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_I_US_APR2013_CRDT_549_4.99";
+        };
+        "TPFTC_I_US_APR2014_UNLCALLUS_1YR_29.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_US_APR2014_UNLCALLUS_1YR_29.99";
+        };
+        "TPFTC_I_US_JAN2014_UNLCALLUS_1MO_2.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_US_JAN2014_UNLCALLUS_1MO_2.99";
+        };
+        "TPFTC_I_US_JUL2014_NUMLOCK_1YR_1.99" =         {
+            entitlements =             (
+                numlock
+            );
+            "product_identifier" = "TPFTC_I_US_JUL2014_NUMLOCK_1YR_1.99";
+        };
+        "TPFTC_I_US_JUL2014_UNLCALLMXALL_1MO_29.99" =         {
+            entitlements =             (
+                "unlimited_calling_mx"
+            );
+            "product_identifier" = "TPFTC_I_US_JUL2014_UNLCALLMXALL_1MO_29.99";
+        };
+        "TPFTC_I_US_JUL2014_UNLCALLUS_1MO_1.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_US_JUL2014_UNLCALLUS_1MO_1.99";
+        };
+        "TPFTC_I_US_JUL2014_UNLCALLWW_1MO_19.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_I_US_JUL2014_UNLCALLWW_1MO_19.99";
+        };
+        "TPFTC_I_US_JUN2014_ADFREEUNLCALLUS_1MO_3.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_I_US_JUN2014_ADFREEUNLCALLUS_1MO_3.99";
+        };
+        "TPFTC_I_US_JUN2014_ADFREE_1MO_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_I_US_JUN2014_ADFREE_1MO_0.99";
+        };
+        "TPFTC_I_US_MAY2014_UNLCALLMX_1MO_6.99" =         {
+            entitlements =             (
+                "unlimited_calling_mx_landline"
+            );
+            "product_identifier" = "TPFTC_I_US_MAY2014_UNLCALLMX_1MO_6.99";
+        };
+        "TPFTC_I_US_MAY2014_UNLCALLUS_1MO_4.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_US_MAY2014_UNLCALLUS_1MO_4.99";
+        };
+        "TPFTC_I_US_MAY2014_UNLCALLUS_1YR_19.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_US_MAY2014_UNLCALLUS_1YR_19.99";
+        };
+        "TPFTC_I_US_NOV2014_UNLCALLUS_1YR_9.99" =         {
+            entitlements =             (
+                "unlimited_calling_us_ca"
+            );
+            "product_identifier" = "TPFTC_I_US_NOV2014_UNLCALLUS_1YR_9.99";
+        };
+        "TPFTC_US_JAN2013_CRDT_1099_9.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_US_JAN2013_CRDT_1099_9.99";
+        };
+        "TPFTC_US_JAN2013_CRDT_199_1.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_US_JAN2013_CRDT_199_1.99";
+        };
+        "TPFTC_US_JAN2013_CRDT_2499_19.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_US_JAN2013_CRDT_2499_19.99";
+        };
+        "TPFTC_US_JAN2013_CRDT_99_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_US_JAN2013_CRDT_99_0.99";
+        };
+        "TPFTC_US_OCT2012_MINS_100_1.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_US_OCT2012_MINS_100_1.99";
+        };
+        "TPFTC_US_OCT2012_MINS_1300_19.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_US_OCT2012_MINS_1300_19.99";
+        };
+        "TPFTC_US_OCT2012_MINS_40_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_US_OCT2012_MINS_40_0.99";
+        };
+        "TPFTC_US_OCT2012_MINS_550_9.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TPFTC_US_OCT2012_MINS_550_9.99";
+        };
+        "TPI_AD_FREE_MONTHLY_V1" =         {
+            entitlements =             (
+                "ad_free"
+            );
+            "product_identifier" = "TPI_AD_FREE_MONTHLY_V1";
+        };
+        "TPI_AD_FREE_YEARLY_V1" =         {
+            entitlements =             (
+                "ad_free"
+            );
+            "product_identifier" = "TPI_AD_FREE_YEARLY_V1";
+        };
+        "TXTPLS_I_CA_DEC2014_CRDT_1099_9.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_CA_DEC2014_CRDT_1099_9.99";
+        };
+        "TXTPLS_I_CA_DEC2014_CRDT_2199_19.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_CA_DEC2014_CRDT_2199_19.99";
+        };
+        "TXTPLS_I_CA_DEC2014_CRDT_549_4.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_CA_DEC2014_CRDT_549_4.99";
+        };
+        "TXTPLS_I_CA_JUL2016_CREDIT_1.39_1.39" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_CA_JUL2016_CREDIT_1.39_1.39";
+        };
+        "TXTPLS_I_CA_OCT2014_ADFREE_1MONTH_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_CA_OCT2014_ADFREE_1MONTH_0.99";
+        };
+        "TXTPLS_I_CA_OCT2014_ADFREE_1YEAR_5.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_CA_OCT2014_ADFREE_1YEAR_5.99";
+        };
+        "TXTPLS_I_CA_SEP2014_CRDT_199_1.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_CA_SEP2014_CRDT_199_1.99";
+        };
+        "TXTPLS_I_ROW_JUL2016_CREDIT_99_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_ROW_JUL2016_CREDIT_99_0.99";
+        };
+        "TXTPLS_I_ROW_MAR2015_CRDT_1099_9.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_ROW_MAR2015_CRDT_1099_9.99";
+        };
+        "TXTPLS_I_ROW_MAR2015_CRDT_2499_19.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_ROW_MAR2015_CRDT_2499_19.99";
+        };
+        "TXTPLS_I_ROW_MAR2015_CRDT_549_4.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_ROW_MAR2015_CRDT_549_4.99";
+        };
+        "TXTPLS_I_UK_JAN2015_CRDT_549_4.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_UK_JAN2015_CRDT_549_4.99";
+        };
+        "TXTPLS_I_US_APR2015_STKR_HAPPYBUNNY_1.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_APR2015_STKR_HAPPYBUNNY_1.99";
+        };
+        "TXTPLS_I_US_DEC2014_CRDT_1099_9.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_DEC2014_CRDT_1099_9.99";
+        };
+        "TXTPLS_I_US_DEC2014_CRDT_2499_19.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_DEC2014_CRDT_2499_19.99";
+        };
+        "TXTPLS_I_US_DEC2014_CRDT_549_4.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_DEC2014_CRDT_549_4.99";
+        };
+        "TXTPLS_I_US_JAN2016_RSKY_3_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_JAN2016_RSKY_3_0.99";
+        };
+        "TXTPLS_I_US_JUL2016_CREDIT_99_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_JUL2016_CREDIT_99_0.99";
+        };
+        "TXTPLS_I_US_MAR2015_STKR_LOL_FREE" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_MAR2015_STKR_LOL_FREE";
+        };
+        "TXTPLS_I_US_MAR2015_STKR_NEX_FREE" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_MAR2015_STKR_NEX_FREE";
+        };
+        "TXTPLS_I_US_MAR2015_STKR_NUMBEAR_FREE" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_MAR2015_STKR_NUMBEAR_FREE";
+        };
+        "TXTPLS_I_US_MAR2015_STKR_SUZYSMART_FREE" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_MAR2015_STKR_SUZYSMART_FREE";
+        };
+        "TXTPLS_I_US_OCT2014_ADFREE_1MONTH_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_OCT2014_ADFREE_1MONTH_0.99";
+        };
+        "TXTPLS_I_US_OCT2014_ADFREE_1YEAR_5.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_OCT2014_ADFREE_1YEAR_5.99";
+        };
+        "TXTPLS_I_US_SEP2014_CRDT_199_1.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTPLS_I_US_SEP2014_CRDT_199_1.99";
+        };
+        "TXTP_AIPX_V01.00_EN.BC1_C" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTP_AIPX_V01.00_EN.BC1_C";
+        };
+        "TXTP_AIPX_V01.00_EN.RA1" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTP_AIPX_V01.00_EN.RA1";
+        };
+        "TXTP_AIPX_V01.00_EN.TN_S365" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTP_AIPX_V01.00_EN.TN_S365";
+        };
+        "TXTP_AIPX_V01.01_EN.SP1_N_Animal" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTP_AIPX_V01.01_EN.SP1_N_Animal";
+        };
+        "TXTP_AIPX_V01.01_EN.SP1_N_Fun" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTP_AIPX_V01.01_EN.SP1_N_Fun";
+        };
+        "TXTP_AIPX_V01.01_EN.SP1_N_SciFi" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "TXTP_AIPX_V01.01_EN.SP1_N_SciFi";
+        };
+        "TXT_I_CA_JAN2022_NUMLOCK_1YEAR_1.99" =         {
+            entitlements =             (
+                numlock
+            );
+            "product_identifier" = "TXT_I_CA_JAN2022_NUMLOCK_1YEAR_1.99";
+        };
+        "TXT_I_ROW_JUN2021_NUMLOCK_1YEAR_1.99" =         {
+            entitlements =             (
+                numlock
+            );
+            "product_identifier" = "TXT_I_ROW_JUN2021_NUMLOCK_1YEAR_1.99";
+        };
+        "txt_i_row_oct2016_adfree_1month_0.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "txt_i_row_oct2016_adfree_1month_0.99";
+        };
+        "txt_i_row_oct2016_adfree_1year_5.99" =         {
+            entitlements =             (
+            );
+            "product_identifier" = "txt_i_row_oct2016_adfree_1year_5.99";
+        };
+        "txt_i_ww_aug2023_premium_numbers_1month" =         {
+            entitlements =             (
+                premium
+            );
+            "product_identifier" = "txt_i_ww_aug2023_premium_numbers_1month";
+        };
+        "txt_i_ww_aug2023_premium_numbers_1year" =         {
+            entitlements =             (
+                premium
+            );
+            "product_identifier" = "txt_i_ww_aug2023_premium_numbers_1year";
+        };
+        "txt_i_ww_aug2023_premium_numbers_6month" =         {
+            entitlements =             (
+                premium
+            );
+            "product_identifier" = "txt_i_ww_aug2023_premium_numbers_6month";
+        };
+    };
+}
