9c0c3f7a55e24482134911918c227533117ebde5
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-12-11 23:08:38 +0300

    Create chatUncensored.js

---
+++ Rewrite/chatUncensored.js
@@ -0,0 +1,6 @@
+[rewrite_local]
+https://api.adapty.io/api/v1/sdk/analytics/profiles url script-response-body https://raw.githubusercontent.com/sultan1sa/quanx/refs/heads/main/Rewrite/Script/chatUncensored.js
+https://api.adapty.io/api/v1/sdk/in-apps/apple/receipt/validate/ url script-response-body https://raw.githubusercontent.com/sultan1sa/quanx/refs/heads/main/Rewrite/Script/chatUncensored2.js
+
+[mitm]
+hostname = api.adapty.io
