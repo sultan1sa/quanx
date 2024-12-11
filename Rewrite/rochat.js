eddb8da3683d2338ee96eb061303a429c10bb6cb
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-12-11 17:54:20 +0300

    Create rochat.js

---
+++ Rewrite/rochat.js
@@ -0,0 +1,6 @@
+[rewrite_local]
+https://api.rochathub.com/inteco.user.v1.UserRightsService/GetUserRights url script-response-body https://github.com/sultan1sa/Ddd/raw/refs/heads/main/sfh/main1
+https://api.rochathub.com/inteco.user.v1.UserRightsService/GetUserRights url script-response-body https://github.com/sultan1sa/Ddd/raw/refs/heads/main/sfh/main2
+
+[mitm]
+hostname = api.rochathub.com
